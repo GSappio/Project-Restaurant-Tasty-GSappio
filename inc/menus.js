94c156bda95af6cdf3dd32de44d39dd202dbbb9f
Gabriel <gabriel.sappio02@gmail.com>
2022-10-24 10:27:08 -0300

    Menu administration - Changing data via POST

--- inc/menus.js
+++ inc/menus.js
@@ -28,26 +28,51 @@ module.exports = {
 
             fields.photo = `images/${path.parse(files.photo.path).base}`;
 
-            conn.query(`
-                INSERT INTO tb_menus (title, description, price, photo)
-                VALUES(?, ?, ?, ?)
-            `, [
-
+            let query, queryPhoto = '', params = [
                 fields.title,
                 fields.description,
-                fields.price,
-                fields.photo
+                fields.price
+            ];
 
-            ], (err, results)=>{
+            if (files.photo.name) {
+
+                queryPhoto = ',photo = ?';
+
+                 params.push(fields.photo);
+
+            }
+
+            if (parseInt(fields.id) > 0) {
+                
+                params.push(fields.id);
+
+                query = `
+                    UPDATE tb_menus
+                    SET title = ?,
+                        description = ?,
+                        price = ?
+                        ${queryphoto}
+                    WHERE id = ?
+                `;
+
+            } else {
+
+                if(!files.photo.name) {
+                    reject('Envia a foto do prato.');
+                }
+
+                query = `
+                INSERT INTO tb_menus (title, description, price, photo)
+                VALUES(?, ?, ?, ?)
+                `;
+            }
+
+            conn.query(query, params,(err, results)=>{
 
                 if (err) {
-
                     reject(err)
-
                 } else {
-
                     resolve(results);
-
                 }
 
             });
