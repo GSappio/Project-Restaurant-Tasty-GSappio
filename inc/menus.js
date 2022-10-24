03fb07f2c6587a947cd24a0f90e3ed5085177ce2
Gabriel <gabriel.sappio02@gmail.com>
2022-10-24 10:44:26 -0300

    Menu administration - Deleting data

--- inc/menus.js
+++ inc/menus.js
@@ -79,6 +79,28 @@ module.exports = {
 
         });
         
+    },
+
+    delete(id){
+
+            return new Promise((resolve, reject)=>{
+
+                conn.query(`
+                    DELETE FROM  tb_menus WHERE id = ?
+                `) [
+                    id
+                ], (err, results)=>{
+
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(results);
+                    }
+
+                }
+
+        });
+
     }
 
 
